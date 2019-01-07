6716220b5c2113fad48e3710f212b86ade893c69
hkdragon9 <kennyhuang9@berkeley.edu>
2019-01-08 00:57:14 +0900

    Final Update to JokePot.py
     Add notes (clarify usage of parameters, functions.), clear TODOs(edge cases) and beautify.

--- JokePot.py
+++ JokePot.py
@@ -4,6 +4,7 @@ import sys
 import json
 import requests
 
+
 def job_deliver(parameter, punchline):
     '''
         Takes two String values and print them consecutively with a 2-sec interval
@@ -17,6 +18,7 @@ def job_deliver(parameter, punchline):
     print("\n")
     print(punchline)
 
+
 def next_joke():
     '''
         User-interactive function that takes 'next' or 'quit' to proceed:
@@ -27,14 +29,14 @@ def next_joke():
             else asks user's response again.
     '''
     x = input('next or quit?')
-    # TODO: handle the upper/lower cases
-    if x == 'next' or x == 'n':
+    if x.lower() == 'next' or x.lower() == 'n':
         return True
-    elif x == 'quit' or x == 'q':
+    elif x.lower() == 'quit' or x.lower() == 'q':
         return False
     else:
         print("I don't understand, please enter 'next' or 'quit'")
         return next_joke()
+
 
 def deliver_joke_from_file(filename):
     '''
@@ -51,7 +53,6 @@ def deliver_joke_from_file(filename):
         csv_reader = csv.reader(csv_file, delimiter=',')
         first_joke = True  # Maybe there is a more space-efficient way.
         for row in csv_reader:
-            # print(row)
             if first_joke:
                 job_deliver(row[0], row[1])
                 first_joke = False
@@ -62,6 +63,7 @@ def deliver_joke_from_file(filename):
                 break
         time.sleep(1)
         print("\nno more jokes today, go back to study CS")
+
 
 def deliver_joke_from_reddit():
     '''
@@ -75,12 +77,12 @@ def deliver_joke_from_reddit():
             1. the joke must be 'over_18'
             2. the joke must start with 'Why', 'What' or 'How'
 
-        Note: The function does not take care of pagination!
-        Note: there might be a cleaner way to combine this method with the previous one.
+        Note: The function does NOT take care of pagination!
+        Note: there might be a cleaner way to combine this method with deliver_joke_from_file().
             But I prefer this because it is more bug-free and test-friendly.
     '''
-    #get the json of the reddit posts.
-    #Regarding the details of the json, please check the link below.
+    # get the json of the reddit posts.
+    # Regarding the details of the json, please check the link below.
     reddit_posts = requests.get(
         "https://www.reddit.com/r/dadjokes.json", headers={'User-agent': 'your bot 0.1'})
     data = reddit_posts.json()
@@ -89,8 +91,8 @@ def deliver_joke_from_reddit():
     for row in data["data"]["children"]:
         title_first_world = row["data"]["title"].split(' ', 1)[0]
 
-        #if not row["data"]["over_18"]:
-        #    continue
+        if row["data"]["over_18"]:
+            continue
         if title_first_world != "Why" and title_first_world != "What" and title_first_world != "How":
             continue
 
@@ -106,36 +108,32 @@ def deliver_joke_from_reddit():
     print("\nno more jokes today, go back to study CS")
 
 
-def deliver_joke_from_reddit_link():
+def deliver_joke_from_reddit_pagination():
     '''
-        If no local file's name is provided,
-        deliver jokes from reddit.
-        Delivers first joke upon called.
-        Only delivers the next joke if the user replies 'next'
-        Quit if user replies 'quit' or no more jokes available.
+        This function works exactly the same as deliver_joke_from_reddit().
+        The only difference is that this function DOES take care of pagination!
 
-        Delivers jokes with 2 more constraints:
-            1. the joke must be 'over_18'
-            2. the joke must start with 'Why', 'What' or 'How'
-
-        Note: The function does not take care of pagination!
-        Note: there might be a cleaner way to combine this method with the previous one.
-            But I prefer this because it is more bug-free and test-friendly.
+        Since Pagination is not required for this project, feel free deliver_joke_from_reddit()
+        instead of this.
     '''
-    #get the json of the reddit posts.
-    #Regarding the details of the json, please check the link below:
+    # get the json of the reddit posts.
+    # Regarding the details of the json, please check the link below:
     # https://www.reddit.com/r/dadjokes.json
 
-    page_number = 0
+    page_number = 0  # Which page we are currently on. Starts at 0.
+    # Refers to the "after" string that is used to transfer to the next page.
     after_string = ""
+    # Used to determine if the user wants to quit, thus terminate while loop.
     quit_jokepot = False
     while(True):
+        # If-Else decides which weblink we should move to.
         if page_number == 0:
             reddit_posts = requests.get(
                 "https://www.reddit.com/r/dadjokes.json", headers={'User-agent': 'your bot 0.1'})
         else:
             reddit_posts = requests.get(
-                f"https://www.reddit.com/r/dadjokes.json?count={25 * page_number}&after={after_string}", headers={'User-agent': 'your bot 0.1'})
+                f"https://www.reddit.com/r/dadjokes.json?count={25 * page_number}&after={after_string}",
+                 headers={'User-agent': 'your bot 0.1'})
 
         data = reddit_posts.json()
         after_string = data["data"]["after"]
@@ -144,10 +142,10 @@ def deliver_joke_from_reddit_link():
         for row in data["data"]["children"]:
             title_first_world = row["data"]["title"].split(' ', 1)[0]
 
-            #if not row["data"]["over_18"]:
-            #    continue
+            if row["data"]["over_18"]:
+                continue
 
-            if title_first_world != "What" and title_first_world != "What" and title_first_world != "What":
+            if title_first_world != "Why" and title_first_world != "What" and title_first_world != "How":
                 continue
 
             if first_joke:
@@ -170,7 +168,7 @@ def deliver_joke_from_reddit_link():
 def main():
     '''
         Main method: to check how many arguments user has provided:
-            One argument - deliver_joke_from_reddit()
+            One argument - deliver_joke_from_reddit_pagination()
             Two arguments - deliver_joke_from_file(*filename)
             More than two - error and quit.
     '''
@@ -181,7 +179,11 @@ def main():
     elif len(sys.argv) == 2:
         deliver_joke_from_file(sys.argv[1])
     else:
-        deliver_joke_from_reddit_link()
+        '''
+        Note: One can change the below function to deliver_joke_from_reddit()
+        if one does not want the pagination functionality.
+        '''
+        deliver_joke_from_reddit_pagination()
 
 
 if __name__ == '__main__':
